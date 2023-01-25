fd804911030f4e851401e2b28f8a1461273e0439
Thian1988 <xthian1988@gmail.com>
2023-01-25 19:17:43 -0300

    Se agrega el proyecto

---
+++ js/eventosjava.js
@@ -0,0 +1,67 @@
+function infoColoreada() {
+    Element =document.getElementById("infopers");
+    Element.src="img/info_touch.png";
+    }
+    function infoComun() {
+        Element =document.getElementById("infopers");
+        Element.src="img/info.png";
+    }
+    
+    function maletinColor() {
+        Element =document.getElementById("explabo");
+        Element.src="img/exp_labo_touch.png";
+    }
+     function maletinOscuro() {
+        Element =document.getElementById("explabo");
+        Element.src="img/exp_labo.png";
+    }
+    
+    function habilidadesEnColor() {
+        Element =document.getElementById("habilidadesycon");
+        Element.src="img/habilidad_touch.png";
+    }
+    function habilidadesEnNegro() {
+        Element =document.getElementById("habilidadesycon");
+        Element.src="img/habilidad.png";
+    }
+    
+    function estudioColor() {
+        Element =document.getElementById("estudios");
+        Element.src="img/estudio_touch.png";
+    }
+    function estudioNegro() {
+        Element =document.getElementById("estudios");
+        Element.src="img/estudio.png";
+    }
+    
+    function faceColor() {
+        Element =document.getElementById("face");
+        Element.src="img/facebook_touch.png";
+    }
+    function faceNegro() {
+        Element =document.getElementById("face");
+        Element.src="img/facebook.png";
+    }
+    
+    function instaColor() {
+        Element =document.getElementById("insta");
+        Element.src="img/instagram_touch.png";
+    }
+    function instaNegro() {
+        Element =document.getElementById("insta");
+        Element.src="img/instagram.png";
+    }
+    function linkedinColor() {
+        Element =document.getElementById("linkedin");
+        Element.src="img/linkedin_touch.png";
+    }
+    function linkedinNegro() {
+        Element =document.getElementById("linkedin");
+        Element.src="img/linkedin.png";
+    }
+
+    var plop= new Audio();
+    plop.src= "img/Plop.mp3"
+
+    var ding= new Audio()
+    ding.src= "img/Ding.mp3"
